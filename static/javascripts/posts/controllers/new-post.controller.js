c59fc27b42ba74456ab8ae693d3504da941b10c2
Arne Riso <arneriso@gmail.com>
2016-10-18 19:58:15 +0300

    18/10/2016

--- static/javascripts/posts/controllers/new-post.controller.js
+++ static/javascripts/posts/controllers/new-post.controller.js
@@ -5,9 +5,9 @@
         .module('thinkster.posts.controllers')
         .controller('NewPostController', NewPostController);
         
-    NewPostController.$inject = ['$rootScope', '$scope', 'Authentication', 'Snackbar', 'Posts', 'ngDialog'];
+    NewPostController.$inject = ['$rootScope', '$scope', 'Authentication', 'Snackbar', 'Posts'];
     
-    function NewPostController($rootScope, $scope, Authentication, Snackbar, Posts, ngDialog) {
+    function NewPostController($rootScope, $scope, Authentication, Snackbar, Posts) {
         var vm = this;
         
         vm.submit = submit;
@@ -19,20 +19,19 @@
                     username: Authentication.getAuthenticatedAccount().username
                 }
             });
-        }
+            
+            $scope.closeThisDialog();
         
-        //$scope.closeThisDialog();
-        ngDialog.close();
-        
-        Posts.create(vm.content).then(createPostSuccessFn, createPostErrorFn);
-        
-        function createPostSuccessFn(data, status, headers, config) {
-            Snackbar.show('Success! Post created.');
-        }
-        
-        function createPostErrorFn(data, status, headers, config) {
-            $rootScope.$broadcast('post.created.error');
-            Snackbar.error(data.error);
+            Posts.create(vm.content).then(createPostSuccessFn, createPostErrorFn);
+            
+            function createPostSuccessFn(data, status, headers, config) {
+                Snackbar.show('Success! Post created.');
+            }
+            
+            function createPostErrorFn(data, status, headers, config) {
+                $rootScope.$broadcast('post.created.error');
+                Snackbar.error(data.error);
+            }
         }
     }
 })();
