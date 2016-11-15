f5d143681cc7f9632b8ed6f9e55a67a011233c18
Arne Riso <arneriso@gmail.com>
2016-11-15 20:21:23 +0200

    puhasta

--- authentication/tests.py
+++ authentication/tests.py
@@ -2,20 +2,15 @@ from django.test import TestCase
 from authentication.models import Account
 from authentication.models import AccountManager
 
+from rest_framework import permissions
+from authentication.permissions import IsAccountOwner
+
 class AccountTestCase(TestCase):
     def setUp(self):
         Account.objects.create(email='user@email.com', username='user')
     
+    # Arne
     def test_user_created(self):
         account = Account.objects.get(username='user')
         self.assertEqual(account.email, 'user@email.com')
         self.assertEqual(account.username, 'user')
-
-    def test_get_full_name(self):
-        Account.objects.create(first_name='esimene', last_name='teine')
-        self.assertEqual(get_full_name(self), 'esimene teine')
-
-    def test_get_short_name(self):
-        Account.objects.create(first_name='esimene')
-        self.assertEqual(get_short_name(self), 'esimene')
-        
--- posts/tests.py
+++ posts/tests.py
@@ -1,9 +1,1 @@
 from django.test import TestCase
-from posts.models import Post
-from authentication.models import Account
-
-'''
-class PostTestCase(TestCase):
-    def setUp(self):
-        Post.objects.create()
-'''
