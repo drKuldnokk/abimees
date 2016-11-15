14b1e4ee0d1bdf9c114aa804e71d8361df3bf3b8
Katre <helena.71@hot.ee>
2016-11-15 08:08:27 +0200

    Unit-testid lisatud

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from authentication.models import Account
+from authentication.models import AccountManager
 
 class AccountTestCase(TestCase):
     def setUp(self):
@@ -9,3 +10,12 @@ class AccountTestCase(TestCase):
         account = Account.objects.get(username='user')
         self.assertEqual(account.email, 'user@email.com')
         self.assertEqual(account.username, 'user')
+
+    def test_get_full_name(self):
+        Account.objects.create(first_name='esimene', last_name='teine')
+        self.assertEqual(get_full_name(self), 'esimene teine')
+
+    def test_get_short_name(self):
+        Account.objects.create(first_name='esimene')
+        self.assertEqual(get_short_name(self), 'esimene')
+        
