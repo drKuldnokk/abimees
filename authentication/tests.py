9b482713eff851d099afa65e18078b89fbcb49bb
Kairit Laht <kairit.laht@gmail.com>
2016-11-15 20:47:11 +0200

    Kairit Commit

--- authentication/tests.py
+++ authentication/tests.py
@@ -14,13 +14,13 @@ class AccountTestCase(TestCase):
         account = Account.objects.get(username='user')
         self.assertEqual(account.email, 'user@email.com')
         self.assertEqual(account.username, 'user')
-        
-    # Kairit
-    def test_get_full_name(self):
-        account = Account.objects.create(first_name='esimene', last_name='teine')
-        self.assertEqual(account.get_full_name(), 'esimene teine')
 	
     # Reemo
     def test_get_short_name(self):
         account = Account.objects.create(first_name='esimene')
         self.assertEqual(account.get_short_name(), 'esimene')
+                
+    # Kairit
+    def test_get_full_name(self):
+        account = Account.objects.create(first_name='esimene', last_name='teine')
+        self.assertEqual(account.get_full_name(), 'esimene teine')
