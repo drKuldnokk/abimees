519f8614abb166d986530ef29d6916e68054c578
Raino Toome <raino70@gmail.com>
2016-11-15 20:57:04 +0200

    Initial Commit

--- posts/tests.py
+++ posts/tests.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+from posts.models import Post
+from authentication.models import Account
+
+class PostTestCase(TestCase):
+    
+    def test_create(self):
+        account = Account.objects.create(email='user@email.com', username='user')
+        post = Post.objects.create(author=account, name="nimi", content="sisu", field="valdkond")
+        self.assertEqual(post.name, "nimi")
