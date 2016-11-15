5561ba18bd1ac5fe81c0c55eb8192add86c6a3b7
Kaur Molder <kaurmldr@gmail.com>
2016-11-15 21:02:23 +0200

    Kaur Commit

--- posts/tests.py
+++ posts/tests.py
@@ -8,3 +8,10 @@ class PostTestCase(TestCase):
         account = Account.objects.create(email='user@email.com', username='user')
         post = Post.objects.create(author=account, name="nimi", content="sisu", field="valdkond")
         self.assertEqual(post.name, "nimi")
+        
+    def test_created_at_filled(self):
+        account = Account.objects.create(email='user@email.com', username='user')
+        post = Post.objects.create(author=account, name="nimi", content="sisu")
+        self.assertTrue(post.created_at)
+    
+    
